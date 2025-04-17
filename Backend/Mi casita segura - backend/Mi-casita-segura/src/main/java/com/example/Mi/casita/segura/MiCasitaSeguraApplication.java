c5b11181abcd5d6f71771eeeb9f6c91d8bd79f41
Hilary Rompich <hrompichp1@miumg.edu.gt>
2025-04-16 18:52:08 -0600

    Corrige estructura: reorganiza backend y frontend

---
+++ Backend/Mi casita segura - backend/Mi-casita-segura/src/main/java/com/example/Mi/casita/segura/MiCasitaSeguraApplication.java
@@ -0,0 +1,13 @@
+package com.example.Mi.casita.segura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiCasitaSeguraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiCasitaSeguraApplication.class, args);
+	}
+
+}
---
+++ Backend/Mi casita segura - backend/Mi-casita-segura/src/test/java/com/example/Mi/casita/segura/MiCasitaSeguraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Mi.casita.segura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiCasitaSeguraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
